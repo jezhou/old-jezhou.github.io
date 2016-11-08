d379208deadd569e205c196beff7192742b9365f
Jesse Zhou <jesse.cy.zhou@gmail.com>
2016-11-08 08:18:10 +0000

    added cname

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import Home from './components/Home'
 import Who from './components/Who'
 import Contact from './components/Contact'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute key="test" component={Home}/>
       <Route path='/who' component={Who} />
